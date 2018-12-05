8a2fa82f5e8b1714e4eae6bb56b3a9a539cab9f6
Arden Rasmussen <ardenisthebest@gmail.com>
2018-12-04 17:49:10 -0800

    04/12/18

--- Fall 2018/Comp/important_sampling.py
+++ Fall 2018/Comp/important_sampling.py
@@ -1,7 +1,18 @@
 import numpy as np
 
+def rand():
+    return np.random.random() ** 2
+
+def f(x):
+    return (1/(np.exp(x)+1))
+
+def w(x):
+    return np.power(x,-0.5)
+
 def main():
-    pass
+    N = int(1e7)
+    I = (1/N)*sum([2.0/(np.exp(rand()) + 1) for i in range(N)])
+    print(I)
 
 if __name__ == "__main__":
     main()
