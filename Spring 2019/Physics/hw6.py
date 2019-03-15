add63c0f02e808363c86522fad798aa6dc643acb
Arden Rasmussen <ardenisthebest@gmail.com>
2019-03-15 16:04:21 -0700

    15/03/19

--- Spring 2019/Physics/hw6.py
+++ Spring 2019/Physics/hw6.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import matplotlob.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 w0=2*np.pi
@@ -8,5 +8,7 @@ v0=0
 dt=0.01
 X=[5, 5]
 for t in np.arange(0,2, dt):
-    X.append()
+    X.append(((1-(w0**2*dt**2)/4)/(1+(w0**2*dt**2)/4))*(2*X[-1]) - X[-2])
 
+plt.plot(np.arange(0,2,dt), X[2:])
+plt.show()
